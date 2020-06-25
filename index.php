0588f84017dae321dcb7ba650ec9f021c1cefd25
Gabriel Becerra <gab.becerra97@protonmail.com>
2020-06-24 22:00:50 -0700

    Sign in

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <title>Just a practice page for PHP</title>
+   </head>
+   <body>
+     <header>
+       <h1>Just a one junk web page more</h1>
+     </header>
+     <div class="body">
+       <ul>
+         <li> <a href="">Log in</a> </li>
+         <li> <a href="Views/signin.html">Sign in</a> </li>
+       </ul>
+     </div>
+   </body>
+ </html>
